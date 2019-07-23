2a8069d7b570d9bfbb46e095bdd6c87b960fb192
Darko Kolak <kolakdarko@yahoo.com>
2019-07-23 10:59:29 +0200

    basic done

---
+++ BasicClass.js
@@ -0,0 +1,22 @@
+class Car{
+
+
+constructor(name,model,makeYear,fuelType,color) {
+
+		this.name = name,
+		this.model = model,
+		this.makeYear = makeYear,
+		this.fuelType = fuelType,
+		this.color = color
+
+		}
+
+
+ color_and_model(){
+ return this.model + " " + this.color
+};
+
+
+
+
+}
